95c231b1f9f6d10e620ff1089f0010cd29eb4085
William <williamlangroudi@gmail.com>
2018-07-10 20:45:04 +0100

    Add header, governance, proposals base componenet data Install all mdbAngular packages Setup Routing for pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -7,20 +9,22 @@ import { HeaderComponent } from './header/header.component';
 import { GovernanceComponent } from './governance/governance.component';
 import { ProposalsComponent } from './proposals/proposals.component';
 
+const appRoutes: Routes = [
+  { path: 'governance', component: GovernanceComponent },
+  { path: 'proposals', component: ProposalsComponent }
+];
+
 @NgModule({
-   declarations: [
-      AppComponent,
-      FooterComponent,
-      HeaderComponent,
-      GovernanceComponent,
-      ProposalsComponent
-   ],
-   imports: [
-      BrowserModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    GovernanceComponent,
+    ProposalsComponent
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes), MDBBootstrapModule.forRoot()],
+  schemas: [NO_ERRORS_SCHEMA],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
