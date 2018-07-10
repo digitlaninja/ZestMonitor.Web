a0752ace26c48eedbe075861e2c5dfac79822a94
William <williamlangroudi@gmail.com>
2018-07-10 20:05:16 +0100

    Added base component skeletons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { GovernanceComponent } from './governance/governance.component';
+import { ProposalsComponent } from './proposals/proposals.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      FooterComponent,
+      HeaderComponent,
+      GovernanceComponent,
+      ProposalsComponent
+   ],
+   imports: [
+      BrowserModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
